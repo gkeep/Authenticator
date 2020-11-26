63cf7c4ef05848112e1b7cfdccde67d3f7191f3f
Gennady Koshkin <gkeep77@protonmail.com>
2020-11-26 14:07:32 +0300

    Remove deprecated function

--- src/algorithm.py
+++ src/algorithm.py
@@ -26,29 +26,6 @@ def database_append(entry):
     with open("data.json", "w") as database:
         database.write(json.dumps(current_db, indent = 4)) # write the new database
 
-def parse_uri(uri, parameter):
-    """Parse the URI for a parameter
-
-    Deprecated?
-    """
-    param = ""
-
-    if parameter == "label":
-        uri = uri[uri.find("//"):]
-        param = uri[uri.find("//")+7 : uri.find(":")]
-    elif parameter == "account":
-        uri = uri[15:]
-        param = uri[uri.find(":") + 1 : uri.find("?")]
-    else:
-        uri = uri[uri.find(parameter):]
-        if parameter == "period":
-            param = uri[uri.find(parameter) + len(parameter) + 1 :]
-        else:
-            param = uri[uri.find(parameter) + len(parameter) + 1 : uri.find("&")]
-
-    param = param.replace('%20', ' ') # remove encoded spaces from URI
-    return param
-
 def get_otp(secret):
     """Return OTP from the passed URI"""
     totp = pyotp.TOTP(secret)
